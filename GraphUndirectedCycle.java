e959f8bd1810b669dfe82447415a77cae95aeea5
Karan Pratap Singh <114873511+karanrajputt@users.noreply.github.com>
2023-05-23 15:58:31 +0530

    Add files via upload

---
+++ GraphUndirectedCycle.java
@@ -0,0 +1,18 @@
+public class GraphUndirectedCycle {
+    private int V;
+    private int LinkedList<Integer>adj[];
+    
+   public GraphUndirectedCycle(int v){
+      V=v;
+      adj=new LinkedList[v];
+      for(int i=0;i<v;i++){
+          adj[i]=new LinkedList<>();
+  
+      }
+  }
+   static void addEdge(int v,int w){
+      adj[v].add(w);   
+  }
+
+  boolean
+}
